f292637471db8eafcbaf2ed58ebe0716702aeb78
itsmeeste <Starlabs337985>
2018-11-11 01:03:20 +0000

    Added an emitter and a fps counter

--- js/ParticleEngine.js
+++ js/ParticleEngine.js
@@ -2,16 +2,25 @@
 
 // Create the particle engine class
 // pass in the startpos of the particle
-var ParticleEngine = function(startPos)
+var ParticleEngine = function(startPos,numOfParticles,lifespam,emitterTime)
 {
     // This will contain a list of particles in for the engine
     this.startPos = startPos;
+    this.currentitr =0;
+    this.numOfParticles = numOfParticles;
+    this.life = lifespam;
     this.particles = [];
+    this.runTime = 0;
+    this.emitterTime = emitterTime;
+    this.addedAll = false;
 };
 
 // A Method to run all of the particles
-ParticleEngine.prototype.run = function()
+ParticleEngine.prototype.run = function(dt)
 {
+    this.runTime += dt;
+
+    this.Emitter();
     /* Go through the list backwards and then call the run function and
        Check to see if the particle has pass over the lifespam
        */
@@ -19,12 +28,31 @@ ParticleEngine.prototype.run = function()
         {
           // Go through and run the particle
             var particle = this.particles[i];
-            particle.run();
+            particle.run(dt);
         } 
         this.removeDead();
 
 };
 
+// if the emitter has a value of 200 then every 5th of a second it will push a new particle from the list
+ParticleEngine.prototype.Emitter = function()
+{
+    if(this.runTime >= (this.currentitr * this.emitterTime) && !this.addedAll)
+    {
+        this.particles.push(
+            new Particle(createVector(this.startPos.x,this.startPos.y),this.life,
+                createVector(random(-1, 1), random(-1, 0)),
+                createVector(0, 0.05)
+            )
+        );
+        this.currentitr++;
+        if(this.currentitr == this.numOfParticles-1)
+        {
+            console.log("added all");
+            this.addedAll = true;
+        }
+    }
+};
 ParticleEngine.prototype.removeDead = function()
 {
     for(var i =this.particles.length -1; i >= 0;--i)
@@ -51,6 +79,9 @@ ParticleEngine.prototype.addParticles = function(num,lifespam)
     }
 };
 
+/*--------------------- Add an emitter to the particle ---------------------------------*/
+
+
 
 /*------------------Particle------------------------------*/
 var Particle = function(pos,life,vel,accel)
@@ -62,12 +93,13 @@ var Particle = function(pos,life,vel,accel)
 };
 
 // now for the methods of the Particle object
-Particle.prototype.update = function()
+Particle.prototype.update = function(dt)
 {
     // need to add the accel to the velocity
-    //this.vel.add(this.accel);
-    //this.pos.add(this.vel);
-    this.life -= 1;
+    //this.vel.add(createVector(this.accel.x * (dt/1000),this.accel.y * (dt/1000)));
+    this.vel.add(this.accel);
+    this.pos.add(this.vel);
+    this.life -= dt;
 };
 
 // This will use the P5 render stuff
@@ -79,10 +111,10 @@ Particle.prototype.render = function()
     ellipse(this.pos.x,this.pos.y,12,12);
 };
 
-Particle.prototype.run = function()
+Particle.prototype.run = function(dt)
 {
     // This will run the render and update methods
-    this.update();
+    this.update(dt);
     this.render();
 };
 
@@ -90,24 +122,47 @@ Particle.prototype.run = function()
 
 
 /*------------------------- P5 render setup --------------------------------*/
+var lastCalledTime;
+var fps;
+
+function FPS() {
+
+  if(!lastCalledTime) {
+     lastCalledTime = Date.now();
+     fps = 0;
+     return;
+  }
+  delta = (Date.now() - lastCalledTime)/1000;
+  lastCalledTime = Date.now();
+  fps = 1/delta;
+}
 
 var particleEngine;
+
+// get the delta time
+var lastUpdate;
 function setup()
 {
     // Need to create the p5 canvas
     createCanvas(800,600);
-    particleEngine = new ParticleEngine(createVector(width/2,50));
-    particleEngine.addParticles(50,50);
+    particleEngine = new ParticleEngine(createVector(width/2,50),300,1000,50);
+    //particleEngine.addParticles(1,1000);
+    console.log("asda");
+    lastUpdate = Date.now();
 };  
 
 
 // This is the draw function that will be ran every frame
 function draw()
 {
+    FPS();
+    console.log("Current FPS: " + Math.round(fps));
     // set the background colour of the canvas
     background(51);
-    particleEngine.run();
-    var size =0;
-    size = particleEngine.particles.length;
-    console.log(size);
+
+    // get the time 
+    var now  = Date.now();
+    var dt = now - lastUpdate;
+    lastUpdate = now;
+    particleEngine.run(dt);
 };
